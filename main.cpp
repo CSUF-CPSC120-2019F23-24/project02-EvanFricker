ce0e416fa8e206e5e72da561fe27f439f8491521
EvanFricker <frickere@csu.fullerton.edu>
2019-09-21 14:58:58 -0700

    Project done

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@
 #include <string>
 #include <iomanip>
 
+
 int main()
  {
    //Delcaring my variables
-
   double days, hotel, meal;
   float  total;
-  std::string location1, location2, location3, location;
+  std::string location;
 
   //Finding out the information of the trip
   std::cout << "Welcome to the Buisness Trip Tracker!!!" << '\n' << '\n';
 
   std::cout << "What is the location of the trip? ",
-  std::cin >> location1 >> location2 >> location3;
+  getline(std::cin, location);
 
   std::cout << "How many days will the trip take? ",
   std::cin >> days;
@@ -32,16 +32,17 @@ int main()
   //Calculation the total
   total = meal + hotel;
 
-  //Combining the location variables
-
-  location = location1 + " " + location2 + " " + location3;
-
-  //Combining the $ with the hotel and meal costs
-
-
+  //Adding the $ to the costs
+  std::string money = "$";
+  std::stringstream mealmoney;
+  mealmoney << money << std::fixed << std::setprecision(2) << meal;
+  std::stringstream hotelmoney;
+  hotelmoney << money << std::fixed << std::setprecision(2) << hotel;
+  std::stringstream totalmoney;
+  totalmoney << money << std::fixed << std::setprecision(2) << total;
 
   //Displaying all the information
-  std::cout << std::setw(15) << "Location" << std::setw(10) << "Days" << std::setw(17) << "Hotel Expenses" << std::setw(17) << "Food Expenses" << std::setw(10) << "Total" << '\n';
-  std::cout << std::setw(15) << location << std::setw(10) << days << std::setw(17) <<  hotel << std::setw(17) <<  meal << std::setw(10) <<  total << '\n';
+  std::cout << std::setw(21) << "Location" << std::setw(10) << "Days" << std::setw(17) << "Hotel Expenses" << std::setw(17) << "Food Expenses" << std::setw(10) << "Total" << '\n';
+  std::cout << std::setw(21) << location.substr(0,20) << std::setw(10) << days << std::setw(17) << hotelmoney.str() << std::setw(17) <<  mealmoney.str() << std::setw(10) <<  totalmoney.str() << '\n';
   return 0;
 }
